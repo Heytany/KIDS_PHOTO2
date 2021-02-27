8839d11be6247108960c67620a37181b737f9d00
Alexsey <aav.18@uni-dubna.ru>
2021-02-27 13:43:29 +0300

    Adaptiv p1

--- js/Swiper_config.js
+++ js/Swiper_config.js
@@ -1,16 +1,16 @@
 var swiper = new Swiper('.image-slider', {
 	spaceBetween: 30,
-	slidesPerView: 3.307,
+	centeredSlides: true,
+	slidesPerView: 3.66, //3.307,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
 	// slidesPerView: 'auto',
-	slidesPerGroup: 1,
+	//slidesPerGroup: 1,
 	//initialSlide: 1,
 	loop: true,
-	centeredSlides: true,
-	watchOverFlow: true,
+	watchOverFlow: false,
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
@@ -18,7 +18,7 @@ var swiper = new Swiper('.image-slider', {
 	slideToClickedSlide: true,
 	effect: 'coverflow',
 	coverflowEffect: {
-		rotate: 15,
+		rotate: 20,
 		stretch: 100,
 		modifier: 1,
 		slideShadows: false,
@@ -32,5 +32,26 @@ var swiper = new Swiper('.image-slider', {
 	mousewheel: {
 		sensitivity: 1,
 		eventsTarget: ".image-slider"
-	}
+	},
+
+/*	breakpoints: {
+		// when window width is >= 360px
+		360: {
+			spaceBetween: 5,
+			slidesPerView: 1.5,
+			centeredSlides: false,
+		},
+		// when window width is >= 768px
+		768: {
+			spaceBetween: 5,
+			slidesPerView: 2.4,
+		},
+		// when window width is >= 900px
+		900: {
+			spaceBetween: 30,
+			slidesPerView: 3.307,
+		}
+	}*/
+
+
 });
