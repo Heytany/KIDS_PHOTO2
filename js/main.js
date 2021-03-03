c9ff5b010bca87969e4b00e74d1d9ac9259f6e16
Alexsey <aav.18@uni-dubna.ru>
2021-03-03 20:41:09 +0300

    asda

--- js/main.js
+++ js/main.js
@@ -61,6 +61,13 @@ $(function () {
 		
 		//</Решение проблемы со скачущими хэдерами и футерами>
 		
+		//<Хэдер>
+		$('.header__burger').on("click", function (e) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('.header__menu').slideToggle(300);
+		})
+		//</Хэдер>
+		
 		//<Сюжеты>
 		//Переключатель фоток
 		$('.plotsm-slider__image').on("click", function () {
@@ -273,10 +280,6 @@ $(function () {
 			});
 		//</Заказать фото>
 		
-	
-	
-	
-	
 		
 	}
 )
